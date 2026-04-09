eeae6f4e647ca521ec5256c592a63d7d3c6c3878
Тимофей Григоренко <yo.tgrig@yandex.ru>
2026-04-09 19:50:36 +0500

    change provider url

--- src/components/InsightStreamOnboarding.tsx
+++ src/components/InsightStreamOnboarding.tsx
@@ -7,7 +7,7 @@ import { setConfigProvider } from '../api';
 
 const INSIGHTSTREAM_PROVIDER = 'openai';
 const INSIGHTSTREAM_MODEL = 'compressa1';
-const INSIGHTSTREAM_HOST = 'https://provider.redsquad.tech';
+const INSIGHTSTREAM_HOST = 'http://bavadim.xyz:8000';
 const INSIGHTSTREAM_BASE_PATH = 'v1/responses';
 
 interface InsightStreamOnboardingProps {
